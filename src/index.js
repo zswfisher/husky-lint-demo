6e6fdf373dc1128bf0541e9d78baefcfd598ab47
zhongshengwei <zhongshengwei@n22.com.cn>
2023-04-13 22:59:39 +0800

    测试5

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const a = '1';
+const a = '13';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
