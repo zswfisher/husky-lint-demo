cb83075c69c7d1fdb101545ce3ce7860934f85a0
zhongshengwei <zhongshengwei@n22.com.cn>
2023-04-15 15:54:55 +0800

    con

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const a = "13dd4445等等54333";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
