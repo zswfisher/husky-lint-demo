df0ca0b475d278e65f4d8fbe7ba8f641cd369b0a
zhongshengwei <zhongshengwei@n22.com.cn>
2023-04-15 16:08:39 +0800

    lint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const a = "13dd4445等等54333"
+const a = "123";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
