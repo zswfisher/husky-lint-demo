1db15a15cb927d3724141ff6c1ea7f2f4ed959a8
zhongshengwei <zhongshengwei@n22.com.cn>
2023-04-15 16:19:33 +0800

    folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const a = "123"
+const a = "1234"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
