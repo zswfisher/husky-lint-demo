ccdc3d6947b49193ba0076660e37c67450d4c53d
zhongshengwei <zhongshengwei@n22.com.cn>
2023-04-15 14:53:12 +0800

    chore: 初始化

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const a = "13dd4";
+const a = "13dd444";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
