f5c7a7c5dba37cdecdc97514b37b09c84e41f6c5
zhongshengwei <zhongshengwei@n22.com.cn>
2023-04-15 15:01:07 +0800

    test commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const a = "13dd444";
+const a = "13dd444554";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
